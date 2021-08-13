2b791f03998ddabec40fdf091ffe6877422e1f8b
gmgercke <gmgercke@zoho.com>
2021-08-13 11:34:38 +0200

    Added basic styling

--- src/Definition.js
+++ src/Definition.js
@@ -1,19 +1,31 @@
 import React from "react";
+import "./Definition.css";
 
 export default function Definition(props) {
   return (
     <div className="Definition">
       <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <p>
-              {definition.definition}
-              <br />
-              <em>{definition.example}</em>
-            </p>
-          </div>
-        );
+        if (definition.example) {
+          return (
+            <div key={index}>
+              <p>
+                {definition.definition}
+                <br />
+                <em>e.g. {definition.example}</em>
+              </p>
+            </div>
+          );
+        } else {
+          return (
+            <div key={index}>
+              <p>
+                {definition.definition}
+                <br />
+              </p>
+            </div>
+          );
+        }
       })}
     </div>
   );
--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Definiton from "./Definition";
+import "./Results.css";
 
 export default function Results(props) {
   if (props.object) {
     return (
       <div className="Results">
         <h2>{props.object.word}</h2>
+        <hr />
         {props.object.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
