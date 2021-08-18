7861e11b2ce8ccc7b8d6f8970a9a0ca5541062a5
gmgercke <gmgercke@zoho.com>
2021-08-18 17:30:03 +0200

    Updated styling

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -6,11 +6,13 @@ export default function Synonyms(props) {
     return (
       <ul className="Synonyms">
         {props.synonyms.map(function (synonym, index) {
-          return (
-            <li className="synonyms-words" key={index}>
-              {synonym}{" "}
-            </li>
-          );
+          if (index < 3) {
+            return (
+              <li className="synonyms-words" key={index}>
+                {synonym}
+              </li>
+            );
+          }
         })}
       </ul>
     );
